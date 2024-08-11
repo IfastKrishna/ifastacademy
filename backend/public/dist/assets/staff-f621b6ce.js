04c3dbebcbaeb2d47cb11052db50e51be3bf30c7
Krishna Kumar <ifastkrishna@gmail.com>
2024-08-11 10:01:15 +0530

    all set

---
+++ backend/public/dist/assets/staff-f621b6ce.js
@@ -0,0 +1,1 @@
+import{j as t,B as f,W as s}from"./index-0e7a8f82.js";function e(){return t.jsxs(f,{children:[t.jsx(s,{children:t.jsx("title",{children:" Staff | IfastAcademy "})}),"Staff"]})}export{e as default};
